159503ed3ff290f673257c8cbdc36f1c222f6b21
eriks.ejubs <eriks.ejubs@gmail.com>
2023-10-06 04:12:56 +0300

    [ASSIGNMENT]: - add springdoc-openapi starter dependency; - define general api information in department-service; - define dto schema descriptions; - define REST API documentation;

--- department-service/src/main/java/com/dxl/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/dxl/departmentservice/DepartmentServiceApplication.java
@@ -1,8 +1,33 @@
 package com.dxl.departmentservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Department Service REST APIs",
+                description = "Department Service REST APIs Documentation",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Eriks Janis",
+                        email = "eriks.ejubs@gmail.com",
+                        url = "https://google.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://google.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Department-Service Doc",
+                url = "https://google.com"
+        )
+)
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
